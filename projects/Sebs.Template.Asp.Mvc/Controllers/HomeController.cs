4e93351ecd933f9c9d4d1b4addd597238fdb2d86
ToSeba <seb.developing@gmail.com>
2023-06-01 14:09:11 +0300

    Added Mvc Template.

---
+++ projects/Sebs.Template.Asp.Mvc/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sebs.Template.Asp.Mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
